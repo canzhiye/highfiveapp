78ddc6bbedfd7b2060274b5590178d50ff29ecec
canzhiye <canzhiye@gmail.com>
2014-01-18 03:07:44 -0500

    ok

--- HighFive/HighFiveFirstViewController.h
+++ HighFive/HighFiveFirstViewController.h
@@ -8,17 +8,26 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreMotion/CoreMotion.h>
+#import <Accounts/Accounts.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface HighFiveFirstViewController : UIViewController <NSURLConnectionDelegate>
+@interface HighFiveFirstViewController : UIViewController <NSURLConnectionDelegate, CLLocationManagerDelegate>
 {
     NSString *screenName;
     NSString *userID;
     
+    NSString *weatherString;
+
+    NSString *receivedScreenName;
+    
     UILabel *usernameLabel;
     UIImageView *profilePicImageView;
+    
+    CLLocationManager *locationManager;
 }
 @property (nonatomic, retain) IBOutlet UILabel *usernameLabel;
 @property (nonatomic, retain) IBOutlet UIImageView *profilePicImageView;
 @property (strong, nonatomic) CMMotionManager *motionManager;
+@property (nonatomic, retain) ACAccount *theAccount;
 
 @end
--- HighFive/HighFiveFirstViewController.m
+++ HighFive/HighFiveFirstViewController.m
@@ -49,6 +49,7 @@
                 // Creating a request to get the info about a user on Twitter
                 SLRequest *twitterInfoRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter requestMethod:SLRequestMethodGET URL:[NSURL URLWithString:@"https://api.twitter.com/1.1/users/show.json"] parameters:[NSDictionary dictionaryWithObject:username forKey:@"screen_name"]];
                 [twitterInfoRequest setAccount:twitterAccount];
+                _theAccount = twitterAccount;
                 
                 // Making the request
                 [twitterInfoRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
@@ -91,12 +92,24 @@
 }
 - (void)viewDidLoad
 {
+    // Create the location manager if this object does not
+    // already have one.
+    if (nil == locationManager)
+    {
+        locationManager = [[CLLocationManager alloc] init];
+    }
+    
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // Set a movement threshold for new events.
+    locationManager.distanceFilter = 500; // meters
+    
+    [locationManager startUpdatingLocation];
+    
     [self fetchUserInfo:@"canzhiye"];
     
-    
-    
-    
-    
+    receivedScreenName = @"jacobvangeffen";
     
     self.motionManager = [[CMMotionManager alloc] init];
     self.motionManager.accelerometerUpdateInterval = .2;
@@ -140,7 +153,8 @@
 -(void)outputAccelertionData:(CMAcceleration)acceleration
 {
    
-    if (acceleration.z > 2 /*some arbitrary threshold*/) {
+    if (acceleration.z > 1.5 /*some arbitrary threshold*/) {
+        
         NSLog(@"z accel: %3.2f",acceleration.z);
         NSLog(@"x accel: %3.2f",acceleration.x);
         NSLog(@"y accel: %3.2f",acceleration.y);
@@ -153,21 +167,25 @@
         //    "velocity",
         //    "uid",
         //    "twitter_id",
-        NSString *timestamp = @"fuck";
-        NSString *loc_x = @"you";
-        NSString *loc_y = @"fuck";
-        NSString *velocity = @"you";
-        NSString *uid = @"fack";
-        NSString *twitter_id = @"u";
         
-        NSString *post = [NSString stringWithFormat:@"&timestamp=%@&loc_x=%@&loc_y=%@&velocity=%@&uid=%@&twitter_id=%@",timestamp,loc_x,loc_y,velocity,uid,twitter_id];
+        
+        double timestamp = [[NSDate date] timeIntervalSince1970];
+
+        CLLocationDegrees loc_x =  locationManager.location.coordinate.latitude;
+        CLLocationDegrees loc_y =  locationManager.location.coordinate.longitude;
+        
+        NSString *velocity = @"fuck";
+        NSString *uid = @"2";
+        NSString *twitter_id = screenName;
+        
+        NSString *post = [NSString stringWithFormat:@"&timestamp=%f&loc_x=%f&loc_y=%f&velocity=%@&uid=%@&twitter_id=%@",timestamp,loc_x,loc_y,velocity,uid,twitter_id];
         
         NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
         NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
         
         
         NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-        [request setURL:[NSURL URLWithString:@"http://10.255.144.69/bump"]];
+        [request setURL:[NSURL URLWithString:@"http://direct.kywu.org/highfive/bump"]];
         [request setHTTPMethod:@"POST"];
         [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
         [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
@@ -188,34 +206,59 @@
             NSLog(@"Connection could not be made");
         }
         
+        NSString *city = @"Detroit";
+        NSURL *getDatWeather = [NSURL URLWithString:[NSString stringWithFormat:@"http://api.wunderground.com/api/dfb228b1988561b2/conditions/q/MI/%@.json",city]];
+        
+        [NSURLConnection sendAsynchronousRequest:[[NSURLRequest alloc] initWithURL:getDatWeather] queue:[[NSOperationQueue alloc]init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
+            
+            if (error) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                return;
+            }
+            // Check if there is some response data
+            if (data) {
+                NSError *error = nil;
+                NSDictionary *weatherData = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&error];
+                // Filter the preferred data
+                NSLog(@"WEATHER RESPONSE: %@",weatherData);
+                NSString *shit = [[weatherData objectForKey:@"current_observation"] objectForKey: @"weather"];
+                
+                weatherString = [NSString stringWithFormat:@"Hey @%@, It is %@ in %@ according to @WeatherAPI", receivedScreenName, shit,city];
+                
+                SLRequest *twitterInfoRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter requestMethod:SLRequestMethodPOST URL:[NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/update.json"] parameters:[NSDictionary dictionaryWithObject:weatherString forKey:@"status"]];
+                [twitterInfoRequest setAccount:_theAccount];
+                
+                // Making the request
+                [twitterInfoRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        // Check if we reached the reate limit
+                        if ([urlResponse statusCode] == 429) {
+                            NSLog(@"Rate limit reached");
+                            return;
+                        }
+                        // Check if there was an error
+                        if (error) {
+                            NSLog(@"Error: %@", error.localizedDescription);
+                            return;
+                        }
+                        // Check if there is some response data
+                        if (responseData) {
+                            
+                            NSError *error = nil;
+                            NSDictionary *TWData = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableLeaves error:&error];
+                            // Filter the preferred data
+                            NSLog(@"MOTHAFUCKIN RESPONSE: %@",TWData);
+                        }
+                    });
+                }];
 
+            }
+            
+        }];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"High Five!" message:@"High Five detected. Now suck each others' dicks." delegate:nil cancelButtonTitle:@"Sweet" otherButtonTitles: nil];
+        [alert show];
     }
-    
-    /*
-    self.accX.text = [NSString stringWithFormat:@" %.2fg",acceleration.x];
-    if(fabs(acceleration.x) > fabs(currentMaxAccelX))
-    {
-        currentMaxAccelX = acceleration.x;
-    }
-    self.accY.text = [NSString stringWithFormat:@" %.2fg",acceleration.y];
-    if(fabs(acceleration.y) > fabs(currentMaxAccelY))
-    {
-        currentMaxAccelY = acceleration.y;
-    }
-    self.accZ.text = [NSString stringWithFormat:@" %.2fg",acceleration.z];
-    if(fabs(acceleration.z) > fabs(currentMaxAccelZ))
-    {
-        currentMaxAccelZ = acceleration.z;
-    }
-    
-    self.maxAccX.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelX];
-    self.maxAccY.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelY];
-    self.maxAccZ.text = [NSString stringWithFormat:@" %.2f",currentMaxAccelZ];
-     */
-}
--(void)outputRotationData:(CMRotationRate)rotation
-{
-    
 }
 
 - (void)didReceiveMemoryWarning
